3e6e125f334510b674dd3c6a582d1a06073c377e
thomaslavalade <thomaslavalade@gmail.com>
2023-09-20 21:03:19 +0200

    pokedex_4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,7 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-function PokemonCard () {
-    const pokemon = pokemonList[0];
-
+function PokemonCard ({pokemon}) {
+  console.log(pokemon);
+    
      return (
       <figure> 
             {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
