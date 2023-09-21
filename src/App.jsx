3c07d083528d6f01f6409497a5b8cab71f1cc8d8
thomaslavalade <thomaslavalade@gmail.com>
2023-09-21 20:52:39 +0200

    pokedex06

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,64 @@
 import './App.css';
 import PokemonCard from "./components/PokemonCard"
+import { useState } from "react";
 
 
 function App() {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-  const pokemon = pokemonList[0];
+
+  const [pokemonList, setPokemonList] = useState([
+    {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "charmander",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      },
+      {
+        name: "squirtle",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      },
+      {
+        name: "pikachu",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      },
+      {
+        name: "mew",
+      },
+    ]);
+
+
+  const pokemon = pokemonList[pokemonIndex];
+  
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+
+  const handleRename = () => {
+    const newPokemonList = [...pokemonList];
+    newPokemonList[pokemonIndex] = {
+      ...newPokemonList[pokemonIndex],
+      name: "Super Pikachu"
+    };
+    setPokemonList(newPokemonList);
+  };
+
+
   return (
     <div>
      <PokemonCard pokemon={pokemon}/>
+     {pokemonIndex > 0 ? <button onClick={() =>setPokemonIndex(pokemonIndex -1)}>Pokemon précedent</button> : null}
+     {pokemonIndex < pokemonList.length - 1 ?  <button onClick={() =>setPokemonIndex(pokemonIndex +1)}>Pokemon suivant</button> : null }
+     <button onClick={handleRename}>Renomme le en : Super Pikachu</button>
     </div>
   );
+
+  
+
 }
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 
 function PokemonCard ({pokemon}) {
-  console.log(pokemon);
     
      return (
       <figure> 
